05cbbad288bcd9d5039514856062c33d753c83c8
Dmytro Larin <d.larin@mascode.com>
2026-06-04 16:03:43 +0300

    Updates

---
+++ assets/NotFoundView-m0xk78cZ.js
@@ -0,0 +1,9 @@
+import{O as e,f as t,k as n}from"./index-E0HTXMMJ.js";import{t as r}from"./BaseImage-CzOche8T.js";n();var i=`/assets/no_content-C1VCnRo2.webp`,a=t(),o=()=>(0,a.jsx)(s,{src:i}),s=e(r)`
+  width: 30rem;
+`,c=()=>(console.log(`Not Found View`),(0,a.jsx)(l,{children:(0,a.jsx)(o,{})})),l=e.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;export{c as default};
